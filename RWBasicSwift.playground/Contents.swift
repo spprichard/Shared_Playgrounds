790943280323070c7f7df0995a1d0cbbf02361aa
Steven Prichard <sprichard@paulbunyan.net>
2016-11-18 10:12:14 -0600

    init commit

---
+++ RWBasicSwift.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func yourName(firstName: String, lastName: String){
+    print("Hello \(firstName) \(lastName)")
+}
+
+
+
+
+yourName(firstName: "Simon", lastName: "Cecere")
+
